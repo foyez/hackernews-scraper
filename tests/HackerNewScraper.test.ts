7224b4d9028c79c5f2c8ed94958ddad0cf18f54e
foyez <foyezar@yahoo.com>
2020-07-01 13:50:10 +0600

    test getTopPosts()'s parameters

--- tests/HackerNewScraper.test.ts
+++ tests/HackerNewScraper.test.ts
@@ -7,12 +7,37 @@ describe('HackerNewsScraper', () => {
     expect(scraper.URL).toBe('https://news.ycombinator.com')
   })
 
-  describe('getTopPosts()', () => {
-    it('should throw an error when parameter is not in range from 1 to 100', () => {
-      const errorMsg =
-        'Number of posts should be a positive integer and less than or equal to 100'
+  describe('getTopPosts() - test for valid parameters', () => {
+    const errorMsg =
+      'Number of posts should be a positive integer and less than or equal to 100'
 
-      expect(scraper.getTopPosts(0)).toThrow(errorMsg)
+    // it('should throw an error when parameter is not given', async () => {
+    //   await expect(scraper.getTopPosts()).rejects.toEqual(errorMsg)
+    // })
+
+    it('should throw an error when parameter is a 0', async () => {
+      expect.assertions(1)
+      await expect(scraper.getTopPosts(0)).rejects.toEqual(errorMsg)
+    })
+
+    it('should throw an error when parameter is a negative number', async () => {
+      expect.assertions(1)
+      await expect(scraper.getTopPosts(-1)).rejects.toEqual(errorMsg)
+    })
+
+    it('should throw an error when parameter is greater than 100', async () => {
+      expect.assertions(1)
+      await expect(scraper.getTopPosts(101)).rejects.toEqual(errorMsg)
+    })
+
+    it('should throw an error when parameter is fraction number', async () => {
+      expect.assertions(1)
+      await expect(scraper.getTopPosts(5.6)).rejects.toEqual(errorMsg)
+    })
+
+    it('should throw an error when parameter is a invalid number', async () => {
+      expect.assertions(1)
+      await expect(scraper.getTopPosts(NaN)).rejects.toEqual(errorMsg)
     })
   })
 })
